5029b84b5c091ad2d69a33c6f835ae6b7dc5def6
ledohy <ledohy6054@gmail.com>
2026-07-28 16:04:03 +0900

    3-2 complete

--- study/RL/ch03_02_FrozenLake_Policy_iteration.py
+++ study/RL/ch03_02_FrozenLake_Policy_iteration.py
@@ -7,4 +7,32 @@ def policy_iteration(env):
     V = np.zeros(env.observation_space.n)
     pi = [0 for i in range(env.observation_space.n)]
     while True:
-        oldV = V.copy()
+        while True:
+            oldV = V.copy()
+            for state in range(env.observation_space.n):
+                action = pi[state]
+                prob, next_state, reward, terminated = env.unwrapped.P[state][action][0]
+                v = reward + gamma*V[next_state]
+                V[state] = v
+                if max(np.abs(V-oldV)) < 1e-8:
+                    break
+
+        converged = True
+        for state in range(env.observation_space.n):
+            old_action = pi[state]
+            q = np.zeros(env.action_space.n)
+            for action in range(env.action_space.n):
+                prob, next_state, reward, terminated = env.unwrapped.P[state][action][0]
+                q[action] = reward + gamma*V[next_state]
+            pi[state] = np.argmax(q)
+            if pi[state] != old_action:
+                converged = False
+        if converged:
+            break
+    return V, pi
+
+env = gym.make("FrozenLake-v1", is_slippery=False, render_mode="ansi")
+
+V, pi = policy_iteration(env)
+print("최적 정책:\n", np.array(pi).reshape([4,4]))
+print("최적 가치 함수:\n", np.round(V.reshape([4,4]),4))
